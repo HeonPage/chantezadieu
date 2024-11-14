a550dfd71caa6376659e94a38cd6b9d958436e06
gkddyd <gleeclub.dev@gmail.com>
2024-11-14 15:37:38 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: NextConfig = {  compiler: {
+    styledComponents: true,
+  },
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "glee.or.kr",
+        pathname: "/files/attach/**",
+      },
+      { protocol: "https", hostname: "www.yonseigleeclub.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "http", hostname: "www.kmc1958.or.kr" },
+      { protocol: "https", hostname: "media.discordapp.net" },
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+      },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "i.imgur.com" },
+      {
+        protocol: "https",
+        hostname: "exzaepsdlwxhvfnrgiri.supabase.co",
+      },
+    ],
+  },
+  /* config options here */ experimental: {
+    serverComponentsExternalPackages: ["sharp"],
+  },
 };
 
 export default nextConfig;
